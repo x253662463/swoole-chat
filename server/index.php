9222defc586132843a11e367a4b049f30d0bf5d4
谢三少 <xieyanjun1992@163.com>
2019-05-08 18:09:42 +0800

    初始化简易版聊天

---
+++ server/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$server = new Swoole\WebSocket\Server("0.0.0.0", 9501);
+
+$server->on('open', function (Swoole\WebSocket\Server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";
+});
+$server->on('message', function (Swoole\WebSocket\Server $server, $frame) {
+    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    foreach ($server->connections as $fd) {
+        if ($server->isEstablished($fd)) {
+            $server->push($fd, $frame->data);
+        }
+    }
+});
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+$server->on('request', function (Swoole\Http\Request $request, Swoole\Http\Response $response) {
+    global $server;//调用外部的server
+    // $server->connections 遍历所有websocket连接用户的fd，给所有用户推送
+    foreach ($server->connections as $fd) {
+        // 需要先判断是否是正确的websocket连接，否则有可能会push失败
+        if ($server->isEstablished($fd)) {
+            $server->push($fd, $request->get['message']);
+        }
+    }
+});
+$server->start();
